8ff12c0985684bca3b89fc2c7ea0e9b230e4b895
Gökhan <gokhanozdemiir3@gmail.com>
2026-02-15 16:57:19 +0300

    Update settings.py

--- backendd/backendd/settings.py
+++ backendd/backendd/settings.py
@@ -75,11 +75,11 @@ WSGI_APPLICATION = "backendd.wsgi.application"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'yazlab_3',
-        'USER': 'postgres',
-        'PASSWORD': '12345',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': '****',
+        'USER': '****',
+        'PASSWORD': '****',
+        'HOST': '****',
+        'PORT': '****',
     }
 }
 
